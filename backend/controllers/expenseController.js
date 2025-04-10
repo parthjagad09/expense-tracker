105ef265e4d4ae2aba32472d1c5f74998be60343
parthjagad09 <pvj911911@gmail.com>
2025-04-10 19:37:57 +0530

    first commit

---
+++ backend/controllers/expenseController.js
@@ -0,0 +1,73 @@
+const xlsx = require("xlsx");
+const Expense = require("../models/Expense");
+
+//add expense in database
+exports.addExpense = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const { icon, category, amount, date } = req.body;
+
+    if (!icon || !category || !amount) {
+      res.status(400).json({ message: "All fields are required." });
+    }
+    const newExpense = new Expense({
+      userId,
+      icon,
+      category,
+      amount,
+      date: new Date(date),
+    });
+
+    await newExpense.save();
+    res.status(200).json(newExpense);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error -> ", error: error.message });
+  }
+};
+
+//get all expense data from database
+exports.getAllExpense = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const expense = await Expense.find({ userId }).sort({ date: -1 });
+    res.json(expense);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error." });
+  }
+};
+
+//delete expense data from database
+exports.deleteExpense = async (req, res) => {
+  try {
+    await Expense.findOneAndDelete(req.params.id);
+    res.status(200).json({ message: "Income Data deleted Successfully." });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error." });
+  }
+};
+
+//download expense data in excel format
+exports.downloadExpenseExcel = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const expense = await Expense.find({ userId }).sort({ date: -1 });
+
+    //prepare data for excel
+    const data = expense.map((item) => ({
+      Category: item.category,
+      Amount: item.amount,
+      Date: item.date,
+    }));
+
+    const wb = xlsx.utils.book_new();
+    const ws = xlsx.utils.json_to_sheet(data);
+
+    xlsx.utils.book_append_sheet(wb, ws, "Expense");
+    xlsx.writeFile(wb, "expense_details.xlsx");
+    res.download("expense_details.xlsx");
+  } catch (error) {
+    res.status(500).json({ message: "Server Error." });
+  }
+};
